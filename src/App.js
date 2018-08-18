dc9348a359e65c66bee29aa5fb9740009e5d3568
Sosodope <sonadolkar93@hotmail.com>
2018-08-19 03:12:59 +0700

    set up components and prepare extension

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    img: [],
+    url: "",
+    loading: true
+  };
+  componentDidMount() {
+    const APP_ID =
+      "b81824007a16b43e9eb7a1344d78513e03ffa9d7691c27c86668f6ae5c1890dd";
+
+    fetch("https://api.unsplash.com/photos/random/?client_id=" + APP_ID)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          img: data,
+          url: data.urls.full,
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log("Error happened during fetching!", err);
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="main-content">
+        {this.state.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <img src={this.state.url} key={this.state.img.id} />
+        )}
       </div>
     );
   }
